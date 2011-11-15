253d99f1a7e86a91f487e5f0fd316a1480bc34ad
Laurent Modolo <laurent@modolo.fr>
2011-11-15 11:19:10 +0100

    first commit

---
+++ timemachine.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+## my own rsync-based snapshot-style backup procedure
+## (cc) marcio rps AT gmail.com
+
+## Modified by Laurent Modolo to work on ssh
+
+# config vars
+
+SRC="/home/laurent/" #dont forget trailing slash!
+SNAP="/backup/geno_pop/modolo/home"
+OPTS="-e ssh -rlti --delay-updates --delete --chmod=a-w"
+MINCHANGES=20
+
+# run this process with real low priority
+
+ionice -c 3 -p $$
+renice +12  -p $$
+
+# sync
+
+rsync $OPTS $SRC sav:$SNAP/latest >> $SRC"rsync.log"
+
+# check if enough has changed and if so
+# make a hardlinked copy named as the date
+
+COUNT=$( wc -l $SRC"rsync.log"|cut -d" " -f1 )
+if [ $COUNT -gt $MINCHANGES ] ; then
+	DATETAG=$(date +%Y-%m-%d)
+	if [ ! -e $SNAP/$DATETAG ] ; then
+		scp $SRC"rsync.log" sav:$SNAP/
+		ssh sav 'bash -c "cp -lR '$SNAP'/latest '$SNAP'/'$DATETAG'"'
+		ssh sav 'bash -c "mv '$SNAP'/rsync.log '$SNAP'/'$DATETAG'/rsync.log"'
+	fi
+fi
